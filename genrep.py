dbc5f7c84dcac4e5d6f4209e2704d32fef0742f1
Joar Warholm <91726192+joarwar@users.noreply.github.com>
2024-09-23 10:47:48 +0200

    Update genrep.py

--- genrep.py
+++ genrep.py
@@ -1,19 +1,6 @@
 #Author: Joar Warholm
-#Date: 2024-09-18
+#Date: 2024-09-25
 
-print("Ei-23 genrep praktiskt prov ht24 ")
-amount_res = int(input("Hur många resistanser? "))
-resistances = []
-pararell = []
-for i in range(amount_res):
-    resistance = float(input(f"Resistans {i + 1}: "))
-    resistances.append(resistance)
-    pararell.append(1/resistance)
-serie_resistance = sum(resistances)
-pararell_resistance = sum(pararell)
-print(pararell)
-print(f"Serieresistans: {serie_resistance}")
-print(f"Pararellresistans: {1/pararell_resistance}")
-
+print("Välkommen till praktiskt prov för Inbyggda System 2 ")
 
 
